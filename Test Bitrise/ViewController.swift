fea232ec088dc12df8897229c8060869ec72b88e
斎藤克哉 <katsuya12321@gmail.com>
2019-07-24 10:39:03 +0900

    Initial Commit

---
+++ Test Bitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Bitrise
+//
+//  Created by 斎藤克哉 on 2019/07/24.
+//  Copyright © 2019 斎藤克哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test BitriseTests/Test_BitriseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_BitriseTests.swift
+//  Test BitriseTests
+//
+//  Created by 斎藤克哉 on 2019/07/24.
+//  Copyright © 2019 斎藤克哉. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Bitrise
+
+class Test_BitriseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
